9ecd8f5c5dabdc3c8307a420bb40703a258970f5
DESKTOP-J2D7394 <50761@example.com>
2026-07-05 20:53:19 +0800

    feat:定义了TileMap万片地图二维数组和类型

---
+++ assets/Levels/Level1/index.ts
@@ -0,0 +1,21 @@
+import { TILE_TYPE_ENUM } from "../Enums";
+import Level1 from "./Level1";
+
+// 定义瓦片接口
+export interface ITile {
+  src: number | null;
+  type: TILE_TYPE_ENUM | null;
+}
+
+// 定义关卡接口
+export interface ILevel {
+  mapInfo: Array<Array<ITile>>;
+  // 其他关卡属性...
+}
+
+// 定义关卡数据
+const levels = {
+  Level1,
+};
+
+export default levels;
